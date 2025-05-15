cc887053d43e44218bc96633aeeaf262e44b5c63
Gregory Edward Caceres Morales <gcaceres@detacoop.cl>
2025-05-15 13:45:08 -0400

    actualización yml con steps de allure cliente y actualizacion de script test:qa-sr para ejecutarse en todo ambiente cross-env

--- scripts/decrypt-env.ts
+++ scripts/decrypt-env.ts
@@ -1,34 +1,4 @@
 /* eslint-disable no-console */
-// scripts/decrypt-env.ts
-// import * as fs from "fs";
-// import * as crypto from "crypto";
-// import * as path from "path";
-// import { glob } from "glob";
-
-// const algorithm = "aes-256-cbc";
-// const key = crypto.scryptSync(process.env.ENV_SECRET_KEY || "", "salt", 32);
-
-// const encFiles = glob.sync(".env.*.enc");
-
-// if (!encFiles.length) {
-//   console.info("❗ No se encontraron archivos .env.*.enc para desencriptar.");
-//   process.exit(0);
-// }
-
-// encFiles.forEach((encPath) => {
-//   const fullPath = path.resolve(encPath);
-//   const outputPath = fullPath.replace(".enc", "");
-//   const content = fs.readFileSync(fullPath, "utf-8");
-//   const [ivHex, encrypted] = content.split(":");
-//   const iv = Buffer.from(ivHex, "hex");
-
-//   const decipher = crypto.createDecipheriv(algorithm, key, iv);
-//   let decrypted = decipher.update(encrypted, "hex", "utf8");
-//   decrypted += decipher.final("utf8");
-
-//   fs.writeFileSync(outputPath, decrypted);
-//   console.info(`🔓 Desencriptado: ${outputPath}`);
-// });
 import { createDecipheriv, scryptSync } from 'crypto';
 import { readFileSync, writeFileSync, existsSync } from 'fs';
 import { config } from 'dotenv';
--- scripts/encrypt-env.ts
+++ scripts/encrypt-env.ts
@@ -1,33 +1,4 @@
 /* eslint-disable no-console */
-// // scripts/encrypt-env.ts
-// import * as fs from "fs";
-// import * as crypto from "crypto";
-// import * as path from "path";
-// import { glob } from "glob";
-
-// const algorithm = "aes-256-cbc";
-// const key = crypto.scryptSync(process.env.ENV_SECRET_KEY || "", "salt", 32);
-
-// const envFiles = glob.sync(".env.*", { ignore: ["*.enc"] });
-
-// if (!envFiles.length) {
-//   console.info("❗ No se encontraron archivos .env.* para encriptar.");
-//   process.exit(0);
-// }
-
-// envFiles.forEach((envPath) => {
-//   const fullPath = path.resolve(envPath);
-//   const outputPath = `${fullPath}.enc`;
-//   const iv = crypto.randomBytes(16);
-
-//   const content = fs.readFileSync(fullPath, "utf-8");
-//   const cipher = crypto.createCipheriv(algorithm, key, iv);
-//   let encrypted = cipher.update(content, "utf8", "hex");
-//   encrypted += cipher.final("hex");
-
-//   fs.writeFileSync(outputPath, iv.toString("hex") + ":" + encrypted);
-//   console.info(`✅ Encriptado: ${outputPath}`);
-// });
 import { createCipheriv, randomBytes, scryptSync } from 'crypto';
 import { readFileSync, writeFileSync, existsSync } from 'fs';
 import { config } from 'dotenv';
